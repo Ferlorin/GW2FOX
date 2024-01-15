ebb146c047da92b8bca80b19bdc98a4e938337ad
Daniel Jürgen Timpe <daniel.j.timpe@gmail.com>
2024-01-15 12:21:49 +0100

    Sry 4 the Chaos

---
+++ GW2FOX/BossEvent.cs
@@ -0,0 +1,17 @@
+﻿namespace GW2FOX
+{
+    public class BossEvent
+    {
+        public string BossName { get; }
+        public TimeSpan Timing { get; }
+        public string Category { get; }
+        public TimeSpan Duration { get; set; }
+
+        public BossEvent(string bossName, string timing, string category)
+        {
+            BossName = bossName;
+            Timing = TimeSpan.Parse(timing);
+            Category = category;
+        }
+    }
+}
