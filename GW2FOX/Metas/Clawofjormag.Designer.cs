0b84b6df971d5d22b27dad1bc3614e9a62c85683
Catnoid <daniel.j.timpe@gmail.com>
2025-04-14 11:48:12 +0200

    beer shopping break

--- GW2FOX/Metas/Clawofjormag.Designer.cs
+++ GW2FOX/Metas/Clawofjormag.Designer.cs
@@ -49,262 +49,258 @@
             Runinfo = new TextBox();
             button6 = new Button();
             Clawachi2 = new TextBox();
-            textBox1 = new TextBox();
-            pictureBox3 = new PictureBox();
-            pictureBox4 = new PictureBox();
             pictureBox2 = new PictureBox();
-            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             SuspendLayout();
             // 
             // button13
             // 
-            button13.Location = new Point(11, 676);
+            button13.BackgroundImage = (Image)resources.GetObject("button13.BackgroundImage");
+            button13.BackgroundImageLayout = ImageLayout.Center;
+            button13.Cursor = Cursors.Cross;
+            button13.FlatStyle = FlatStyle.Flat;
+            button13.Location = new Point(846, 340);
             button13.Name = "button13";
             button13.Size = new Size(75, 23);
             button13.TabIndex = 117;
-            button13.Text = "Copy";
             button13.UseVisualStyleBackColor = true;
             button13.Click += Clawachi1_Click;
             // 
             // Clawachi1
             // 
-            Clawachi1.Font = new Font("Segoe UI", 15F);
-            Clawachi1.Location = new Point(11, 610);
+            Clawachi1.BackColor = Color.Red;
+            Clawachi1.Font = new Font("Segoe UI", 9F);
+            Clawachi1.Location = new Point(490, 324);
             Clawachi1.Multiline = true;
             Clawachi1.Name = "Clawachi1";
             Clawachi1.ReadOnly = true;
             Clawachi1.ScrollBars = ScrollBars.Vertical;
-            Clawachi1.Size = new Size(600, 60);
+            Clawachi1.Size = new Size(350, 39);
             Clawachi1.TabIndex = 116;
             Clawachi1.Text = "☠ Achievement \"Crashlanding\": Stack on Comi and wait!\r\n\r\n";
             // 
             // button9
             // 
-            button9.Location = new Point(1294, 505);
+            button9.BackgroundImage = (Image)resources.GetObject("button9.BackgroundImage");
+            button9.BackgroundImageLayout = ImageLayout.Center;
+            button9.Cursor = Cursors.Cross;
+            button9.FlatStyle = FlatStyle.Flat;
+            button9.Location = new Point(371, 430);
             button9.Name = "button9";
             button9.Size = new Size(75, 23);
             button9.TabIndex = 115;
-            button9.Text = "Copy";
             button9.UseVisualStyleBackColor = true;
             button9.Click += Attentionclaw_Click;
             // 
             // button8
             // 
-            button8.Location = new Point(654, 505);
+            button8.BackgroundImage = (Image)resources.GetObject("button8.BackgroundImage");
+            button8.BackgroundImageLayout = ImageLayout.Center;
+            button8.Cursor = Cursors.Cross;
+            button8.FlatStyle = FlatStyle.Flat;
+            button8.Location = new Point(371, 385);
             button8.Name = "button8";
             button8.Size = new Size(75, 23);
             button8.TabIndex = 114;
-            button8.Text = "Copy";
             button8.UseVisualStyleBackColor = true;
             button8.Click += Clawinstance_Click;
             // 
             // button7
             // 
-            button7.Location = new Point(13, 505);
+            button7.BackgroundImage = (Image)resources.GetObject("button7.BackgroundImage");
+            button7.BackgroundImageLayout = ImageLayout.Center;
+            button7.Cursor = Cursors.Cross;
+            button7.FlatStyle = FlatStyle.Flat;
+            button7.Location = new Point(371, 340);
             button7.Name = "button7";
             button7.Size = new Size(75, 23);
             button7.TabIndex = 113;
-            button7.Text = "Copy";
             button7.UseVisualStyleBackColor = true;
             button7.Click += Clawinfo_Click;
             // 
             // button5
             // 
-            button5.Location = new Point(1440, 374);
+            button5.BackgroundImage = (Image)resources.GetObject("button5.BackgroundImage");
+            button5.BackgroundImageLayout = ImageLayout.Center;
+            button5.Cursor = Cursors.Cross;
+            button5.FlatStyle = FlatStyle.Flat;
+            button5.Location = new Point(1796, 295);
             button5.Name = "button5";
             button5.Size = new Size(75, 23);
             button5.TabIndex = 112;
-            button5.Text = "Copy";
             button5.UseVisualStyleBackColor = true;
             button5.Click += Welcome_Click;
             // 
             // button4
             // 
-            button4.Location = new Point(965, 374);
+            button4.BackgroundImage = (Image)resources.GetObject("button4.BackgroundImage");
+            button4.BackgroundImageLayout = ImageLayout.Center;
+            button4.Cursor = Cursors.Cross;
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.Location = new Point(1321, 295);
             button4.Name = "button4";
             button4.Size = new Size(75, 23);
             button4.TabIndex = 111;
-            button4.Text = "Copy";
             button4.UseVisualStyleBackColor = true;
             button4.Click += Guild_Click;
             // 
             // button3
             // 
-            button3.Location = new Point(490, 374);
+            button3.BackgroundImage = (Image)resources.GetObject("button3.BackgroundImage");
+            button3.BackgroundImageLayout = ImageLayout.Center;
+            button3.Cursor = Cursors.Cross;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Location = new Point(846, 295);
             button3.Name = "button3";
             button3.Size = new Size(75, 23);
             button3.TabIndex = 110;
-            button3.Text = "Copy";
             button3.UseVisualStyleBackColor = true;
             button3.Click += Squadinfo_Click;
             // 
             // button2
             // 
-            button2.Location = new Point(15, 374);
+            button2.BackgroundImage = (Image)resources.GetObject("button2.BackgroundImage");
+            button2.BackgroundImageLayout = ImageLayout.Center;
+            button2.Cursor = Cursors.Cross;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Location = new Point(371, 295);
             button2.Name = "button2";
             button2.Size = new Size(75, 23);
             button2.TabIndex = 109;
-            button2.Text = "Copy";
             button2.UseVisualStyleBackColor = true;
             button2.Click += Runinfo_Click;
             // 
             // Attentionclaw
             // 
-            Attentionclaw.Font = new Font("Segoe UI", 15F);
-            Attentionclaw.Location = new Point(1294, 439);
+            Attentionclaw.Font = new Font("Segoe UI", 9F);
+            Attentionclaw.Location = new Point(15, 414);
             Attentionclaw.Multiline = true;
             Attentionclaw.Name = "Attentionclaw";
             Attentionclaw.ReadOnly = true;
             Attentionclaw.ScrollBars = ScrollBars.Vertical;
-            Attentionclaw.Size = new Size(600, 60);
+            Attentionclaw.Size = new Size(350, 39);
             Attentionclaw.TabIndex = 107;
             Attentionclaw.Text = "≪ATTENTION≫ Meta -Train │ ☠Lvl 80 │ “The Claw of Jormag” with Achievement “Crash-Landing” (I tell you later what to do.) │ ☣[&BHoCAAA=]☣ → West to the Com  - Join us!";
             // 
             // Clawinstance
             // 
-            Clawinstance.Font = new Font("Segoe UI", 15F);
-            Clawinstance.Location = new Point(654, 439);
+            Clawinstance.Font = new Font("Segoe UI", 9F);
+            Clawinstance.Location = new Point(15, 369);
             Clawinstance.Multiline = true;
             Clawinstance.Name = "Clawinstance";
             Clawinstance.ReadOnly = true;
             Clawinstance.ScrollBars = ScrollBars.Vertical;
-            Clawinstance.Size = new Size(600, 60);
+            Clawinstance.Size = new Size(350, 39);
             Clawinstance.TabIndex = 106;
             Clawinstance.Text = "Go out of fight, then right click group 2 & join ☣Frostgorge Sound☣\r\n";
             // 
             // Clawinfo
             // 
             Clawinfo.BackColor = SystemColors.Highlight;
-            Clawinfo.Font = new Font("Segoe UI", 15F);
-            Clawinfo.Location = new Point(14, 439);
+            Clawinfo.Font = new Font("Segoe UI", 9F);
+            Clawinfo.Location = new Point(15, 324);
             Clawinfo.Multiline = true;
             Clawinfo.Name = "Clawinfo";
             Clawinfo.ReadOnly = true;
             Clawinfo.ScrollBars = ScrollBars.Vertical;
-            Clawinfo.Size = new Size(600, 60);
+            Clawinfo.Size = new Size(350, 39);
             Clawinfo.TabIndex = 105;
             Clawinfo.Text = "☠Lvl 80 ≪Claw of Jormag≫ with Achievement \"Crash-Landing\" ☣[&BHoCAAA=]☣\r\n\r\n";
             // 
             // button1
             // 
-            button1.BackgroundImage = Properties.Resources.back;
+            button1.BackgroundImage = (Image)resources.GetObject("button1.BackgroundImage");
+            button1.BackgroundImageLayout = ImageLayout.Center;
+            button1.Cursor = Cursors.Cross;
             button1.FlatStyle = FlatStyle.Flat;
-            button1.Location = new Point(1793, 900);
+            button1.Location = new Point(1864, 1002);
             button1.Name = "button1";
-            button1.Size = new Size(100, 102);
+            button1.Size = new Size(40, 40);
             button1.TabIndex = 104;
-            button1.Text = "button1";
             button1.UseVisualStyleBackColor = true;
             button1.Click += Back_Click;
             // 
             // Symbols
             // 
-            Symbols.Font = new Font("Segoe UI", 15F);
+            Symbols.Font = new Font("Segoe UI", 11F);
             Symbols.Location = new Point(1458, 12);
             Symbols.Multiline = true;
             Symbols.Name = "Symbols";
             Symbols.ReadOnly = true;
             Symbols.ScrollBars = ScrollBars.Vertical;
-            Symbols.Size = new Size(434, 221);
+            Symbols.Size = new Size(429, 85);
             Symbols.TabIndex = 103;
             // 
             // Welcome
             // 
-            Welcome.Font = new Font("Segoe UI", 15F);
+            Welcome.Font = new Font("Segoe UI", 9F);
             Welcome.Location = new Point(1440, 268);
             Welcome.Multiline = true;
             Welcome.Name = "Welcome";
             Welcome.ReadOnly = true;
             Welcome.ScrollBars = ScrollBars.Vertical;
-            Welcome.Size = new Size(450, 100);
+            Welcome.Size = new Size(350, 50);
             Welcome.TabIndex = 102;
             // 
             // Guild
             // 
-            Guild.Font = new Font("Segoe UI", 15F);
+            Guild.Font = new Font("Segoe UI", 9F);
             Guild.Location = new Point(965, 268);
             Guild.Multiline = true;
             Guild.Name = "Guild";
             Guild.ReadOnly = true;
             Guild.ScrollBars = ScrollBars.Vertical;
-            Guild.Size = new Size(450, 100);
+            Guild.Size = new Size(350, 50);
             Guild.TabIndex = 101;
             // 
             // Squadinfo
             // 
-            Squadinfo.Font = new Font("Segoe UI", 15F);
+            Squadinfo.Font = new Font("Segoe UI", 9F);
             Squadinfo.Location = new Point(490, 268);
             Squadinfo.Multiline = true;
             Squadinfo.Name = "Squadinfo";
             Squadinfo.ReadOnly = true;
             Squadinfo.ScrollBars = ScrollBars.Vertical;
-            Squadinfo.Size = new Size(450, 100);
+            Squadinfo.Size = new Size(350, 50);
             Squadinfo.TabIndex = 100;
             Squadinfo.Text = "\r\n";
             // 
             // Runinfo
             // 
-            Runinfo.Font = new Font("Segoe UI", 15F);
+            Runinfo.Font = new Font("Segoe UI", 9F);
             Runinfo.Location = new Point(15, 268);
             Runinfo.Multiline = true;
             Runinfo.Name = "Runinfo";
             Runinfo.ReadOnly = true;
             Runinfo.ScrollBars = ScrollBars.Vertical;
-            Runinfo.Size = new Size(450, 100);
+            Runinfo.Size = new Size(350, 50);
             Runinfo.TabIndex = 99;
             // 
             // button6
             // 
-            button6.Location = new Point(11, 771);
+            button6.BackgroundImage = (Image)resources.GetObject("button6.BackgroundImage");
+            button6.BackgroundImageLayout = ImageLayout.Center;
+            button6.Cursor = Cursors.Cross;
+            button6.FlatStyle = FlatStyle.Flat;
+            button6.Location = new Point(846, 385);
             button6.Name = "button6";
             button6.Size = new Size(75, 23);
             button6.TabIndex = 118;
-            button6.Text = "Copy";
             button6.UseVisualStyleBackColor = true;
             button6.Click += Clawachi2_Click;
             // 
             // Clawachi2
             // 
-            Clawachi2.Font = new Font("Segoe UI", 15F);
-            Clawachi2.Location = new Point(11, 705);
+            Clawachi2.BackColor = Color.Red;
+            Clawachi2.Font = new Font("Segoe UI", 9F);
+            Clawachi2.Location = new Point(490, 369);
             Clawachi2.Multiline = true;
             Clawachi2.Name = "Clawachi2";
             Clawachi2.ReadOnly = true;
-            Clawachi2.Size = new Size(600, 60);
+            Clawachi2.ScrollBars = ScrollBars.Vertical;
+            Clawachi2.Size = new Size(350, 39);
             Clawachi2.TabIndex = 119;
             Clawachi2.Text = "☠ Skyscale Achievement “Get frozen in Ice”: Fight against arrow ↓ and circle ○. DON’T dodge!\r\n";
-            // 
-            // textBox1
-            // 
-            textBox1.BackColor = SystemColors.ScrollBar;
-            textBox1.Font = new Font("Segoe UI", 15F);
-            textBox1.Location = new Point(11, 570);
-            textBox1.Multiline = true;
-            textBox1.Name = "textBox1";
-            textBox1.ReadOnly = true;
-            textBox1.Size = new Size(276, 34);
-            textBox1.TabIndex = 120;
-            textBox1.Text = "Achievements:";
-            // 
-            // pictureBox3
-            // 
-            pictureBox3.Image = Properties.Resources.Line;
-            pictureBox3.Location = new Point(12, 534);
-            pictureBox3.Name = "pictureBox3";
-            pictureBox3.Size = new Size(1880, 30);
-            pictureBox3.TabIndex = 121;
-            pictureBox3.TabStop = false;
-            // 
-            // pictureBox4
-            // 
-            pictureBox4.Image = Properties.Resources.WBsheader2;
-            pictureBox4.Location = new Point(14, 403);
-            pictureBox4.Name = "pictureBox4";
-            pictureBox4.Size = new Size(1877, 30);
-            pictureBox4.TabIndex = 123;
-            pictureBox4.TabStop = false;
             // 
             // pictureBox2
             // 
@@ -321,9 +317,6 @@
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1904, 1041);
             Controls.Add(pictureBox2);
-            Controls.Add(pictureBox4);
-            Controls.Add(pictureBox3);
-            Controls.Add(textBox1);
             Controls.Add(Clawachi2);
             Controls.Add(button6);
             Controls.Add(button13);
@@ -348,8 +341,6 @@
             Name = "Clawofjormag";
             Text = "GW2FOX";
             WindowState = FormWindowState.Maximized;
-            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ResumeLayout(false);
             PerformLayout();
@@ -377,9 +368,6 @@
         private TextBox Runinfo;
         private Button button6;
         private TextBox Clawachi2;
-        private TextBox textBox1;
-        private PictureBox pictureBox3;
-        private PictureBox pictureBox4;
         private PictureBox pictureBox2;
     }
 }
