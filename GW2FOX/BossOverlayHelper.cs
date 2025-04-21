8fd193d47e3a30b2b82b2c00dcdc97fbecdc2446
Catnoid <daniel.j.timpe@gmail.com>
2025-04-21 13:19:08 +0200

    fgdghghsg

--- GW2FOX/BossOverlayHelper.cs
+++ GW2FOX/BossOverlayHelper.cs
@@ -3,40 +3,50 @@ using System.Collections.Generic;
 
 namespace GW2FOX
 {
-    public static class BossOverlayHelper
-    {
-        public static List<BossListItem> GetBossOverlayItems(List<BossEventRun> combinedBosses)
+    using System;
+    using System.Collections.Generic;
+
+ 
+        public static class BossOverlayHelper
         {
-            var overlayItems = new List<BossListItem>();
-
-            foreach (var run in combinedBosses)
+            public static List<BossListItem> GetBossOverlayItems(List<BossEventRun> combinedBosses)
             {
-                var timeRemaining = run.NextRunTime - DateTime.UtcNow;
-                bool isPast = timeRemaining.TotalSeconds < 0;
+                var overlayItems = new List<BossListItem>();
+                var now = DateTime.UtcNow;
 
-                // ⏱ Wenn das Event älter als 14:59 ist, überspringen
-                if (isPast && timeRemaining.TotalMinutes <= -15)
-                    continue;
-
-                if (isPast)
+                foreach (var run in combinedBosses)
                 {
-                    timeRemaining = -timeRemaining;
+                    var timeRemaining = run.NextRunTime - now;
+                    bool isPast = timeRemaining.TotalSeconds < 0;
+
+                    // Vergangene Events: nur anzeigen, wenn sie max. 15 Minuten her sind
+                    if (isPast && timeRemaining.TotalMinutes <= -15)
+                        continue;
+
+                    // Zukünftige Events: nur anzeigen, wenn sie in den nächsten 4 Stunden sind
+                    if (!isPast && timeRemaining.TotalHours > 4)
+                        continue;
+
+                    // Zeit korrekt formatieren
+                    if (isPast)
+                        timeRemaining = -timeRemaining;
+
+                    string formattedTime = isPast
+                        ? timeRemaining.ToString(@"mm\:ss") // vergangen: Minuten:Sekunden
+                        : timeRemaining.ToString(@"hh\:mm\:ss"); // zukünftig: Stunden:Minuten:Sekunden
+
+                    overlayItems.Add(new BossListItem
+                    {
+                        BossName = run.BossName,
+                        Waypoint = run.Waypoint,
+                        IsPastEvent = isPast,
+                        TimeRemainingFormatted = isPast ? "-" + formattedTime : formattedTime
+                    });
                 }
 
-                string formattedTime = isPast
-                    ? timeRemaining.ToString(@"mm\:ss")     // vergangen: nur Minuten+Sekunden
-                    : timeRemaining.ToString(@"hh\:mm\:ss"); // zukünftig: volle Stundenanzeige
-
-                overlayItems.Add(new BossListItem
-                {
-                    BossName = run.BossName,
-                    Waypoint = run.Waypoint,
-                    IsPastEvent = isPast,
-                    TimeRemainingFormatted = isPast ? "-" + formattedTime : formattedTime
-                });
+                return overlayItems;
             }
-
-            return overlayItems;
         }
     }
-}
+
+
